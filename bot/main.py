02e506a1e4f433ae9f94335d8869fd16fb8e847e
Colbey Chittenden <71715990+colbeyp2@users.noreply.github.com>
2023-04-21 18:13:17 -0500

    adding more configurability

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,30 @@
+import argparse
 import discord
 import os
 from dotenv import load_dotenv
 
 async def send_it(message):
     print(f"sending message: {message}")
-    await bot.get_channel(int(os.getenv('DISCORD_REPORTING_CHANNEL_ID'))).send(str(message))
+    await bot.get_channel(int(channelId)).send(str(message))
     return True
 
+async def log_it(message):
+    # send logs to the channel
+    print(f"logging message: {message}")
+    if(DEBUG_LOG == "1"):
+        await send_it("UserEvolutionBot initialized")
+
 load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cid', help="Channel ID to send logs to. If not used, the DISCORD_REPORTING_CHANNEL_ID value in the .env will be used instead. This setting will override the .env")
+parser.add_argument('--debug', help="Whether or not to have debug logs get sent, set this to 1 to send debug logs. If not used, the DEBUG value in the .env will be used instead. This setting will override the .env")
+args = parser.parse_args()
+
+channelId = args.cid or os.getenv('DISCORD_REPORTING_CHANNEL_ID')
+DEBUG_LOG = args.debug or os.getenv('DEBUG')
+
+print(f"sending to {channelId} and debug set to {DEBUG_LOG}")
 
 intents = discord.Intents.default()
 intents.members = True
@@ -15,8 +32,7 @@ bot = discord.Bot(intents=intents)
 
 @bot.event
 async def on_ready():
-    if(os.getenv("DEBUG") is not None and os.getenv("DEBUG") == "1"):
-        await send_it("UserEvolutionBot initialized")
+    log_it("UserEvolutionBot initialized")
 
 @bot.event
 async def on_user_update(before, after):
